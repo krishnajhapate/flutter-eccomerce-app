689770e05c760ccdfb52c7d33823dab4e58cc7e0
krishnajhapate <ki54.jhapate@gmail.com>
2022-01-12 09:07:19 +0530

    font family added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/login_page.dart';
 import 'pages/home_page.dart';
 
@@ -16,7 +17,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
       initialRoute: "/",
       routes: {
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,8 +6,18 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+        color: Colors.white,
         child: Column(
-      children: [Image.asset("assets/images/login_image.png")],
-    ));
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            const Text(
+              "Welcome",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ));
   }
 }
