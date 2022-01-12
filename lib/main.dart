d2eb7e4311cdc780a30fedd41ff9dde3ccb13e93
krishnajhapate <ki54.jhapate@gmail.com>
2022-01-12 08:46:12 +0530

    login page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
 import 'pages/home_page.dart';
 
 void main() {
@@ -12,6 +13,16 @@ class MyApp extends StatelessWidget {
     int days = 0;
     String name = "Krishna Jhapate";
 
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
   }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text("Catalog App"),
+      // ),
+      body: Center(
+        child: Container(
+          child: Text("Login Page "),
+        ),
+      ),
+      // drawer: Drawer(),
+    );
+  }
+}
