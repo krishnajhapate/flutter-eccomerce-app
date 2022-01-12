0d951dc33baefcb36f02f0e02f2e2c7c77ce4a5a
krishnajhapate <ki54.jhapate@gmail.com>
2022-01-12 09:21:39 +0530

    created awesome login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -13,10 +13,46 @@ class LoginPage extends StatelessWidget {
               "assets/images/login_image.png",
               fit: BoxFit.cover,
             ),
+            const SizedBox(
+              height: 20.0,
+            ),
             const Text(
               "Welcome",
               style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Enter username",
+                        labelText: "Username",
+                      ),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        hintText: "Enter Password",
+                        labelText: "Password",
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    ElevatedButton(
+                      child: const Text("Login"),
+                      style: TextButton.styleFrom(),
+                      onPressed: () {
+                        print("object");
+                      },
+                    ),
+                  ],
+                ))
           ],
         ));
   }
