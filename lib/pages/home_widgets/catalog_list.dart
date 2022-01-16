e49d2d3e27584ba5fc6ba7aded4c1ae6232c369c
krishnajhapate <ki54.jhapate@gmail.com>
2022-01-16 16:18:57 +0530

    separated parts

---
+++ lib/pages/home_widgets/catalog_list.dart
@@ -0,0 +1,67 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_catelog/models/catelog.dart';
+import 'package:flutter_catelog/pages/home_widgets/catalog_image.dart';
+import 'package:flutter_catelog/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class CatalogList extends StatelessWidget {
+  const CatalogList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: CatalogModel.items.length,
+        itemBuilder: (context, index) {
+          final catalog = CatalogModel.items[index];
+          return CatalogItems(catalog: catalog).expand();
+        });
+  }
+}
+
+class CatalogItems extends StatelessWidget {
+  final Item catalog;
+
+  const CatalogItems({Key? key, required this.catalog})
+      : assert(catalog != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+        child: Row(
+      children: [
+        CatalogImage(
+          image: catalog.image,
+        ),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            catalog.name.text.lg.color(MyTheme.darkBluishColor).bold.make(),
+            catalog.desc.text.sm.color(Colors.black54).make(),
+            6.heightBox,
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceBetween,
+              buttonPadding: Vx.mH8,
+              children: [
+                "\$${catalog.price}".text.xl.bold.make(),
+                ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(MyTheme.darkBluishColor),
+                        shape:
+                            MaterialStateProperty.all(const StadiumBorder())),
+                    child: "Buy".text.make())
+              ],
+            )
+          ],
+        ))
+      ],
+    )).white.rounded.square(130).make().py16();
+  }
+}
