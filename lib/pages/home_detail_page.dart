35f1ec8d96ed3b5e5a52ec9bcc6f21730f1810e7
krishnajhapate <ki54.jhapate@gmail.com>
2022-01-16 23:28:33 +0530

    added home detail page

---
+++ lib/pages/home_detail_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catelog/models/catelog.dart';
+
+class HomeDetailPage extends StatelessWidget {
+  final Item catalog;
+  const HomeDetailPage({Key? key, required this.catalog}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
